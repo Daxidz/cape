47b35467b251b50ae86f3591bb2ec478a849176d
David Truan <david.truan@heig-vd.ch>
2020-07-07 16:10:22 +0200

    Sprite changed, rotation implemented.

--- Cape2.gd
+++ Cape2.gd
@@ -29,12 +29,17 @@ func current_idx_set(new_idx):
 	if current_idx != IDX_MODES.IDLE and new_idx == IDX_MODES.IDLE:
 		FOLLOW_SPEEDS[0] = TRASNI_FOLLOW_SPEED
 		$Timer.start(1)
+	if current_idx == IDX_MODES.IDLE:
+		time_offset = 2
+	else:
+		time_offset = 0.5
+		
 	current_idx = new_idx
 	
 func current_idx_get():
 	return current_idx
 
-const TIME_OFFSET = 1
+var time_offset = 1
 var cur_time = 0
 
 var last_base_pos = Vector2(0,0)
@@ -43,18 +48,18 @@ func _ready():
 	rng.randomize()
 
 func _physics_process(delta):
-	var mouse_pos = get_global_mouse_position()
-	global_position = mouse_pos
+#	var mouse_pos = get_global_mouse_position()
+#	global_position = mouse_pos
 	
 	cur_time += delta
 
-	if cur_time > TIME_OFFSET:
+	if cur_time > time_offset:
 		cur_time = 0
 		var offset = Vector2()
 
-		offset.x = rng.randi_range(MIN_OFFSET, MAX_OFFSET)
-		offset.y = rng.randi_range(MIN_OFFSET, MAX_OFFSET)
-		follow1.global_position += offset
+#		offset.x = rng.randi_range(MIN_OFFSET, MAX_OFFSET)
+#		offset.y = rng.randi_range(MIN_OFFSET, MAX_OFFSET)
+#		follow1.global_position += offset
 		
 		offset.x = rng.randi_range(MIN_OFFSET, MAX_OFFSET)
 		offset.y = rng.randi_range(MIN_OFFSET, MAX_OFFSET)
@@ -63,9 +68,11 @@ func _physics_process(delta):
 
 	follow1.global_position = follow1.global_position.linear_interpolate(global_position + approch_indexes[current_idx][1], delta * FOLLOW_SPEEDS[current_idx])
 	follow2.global_position = follow2.global_position.linear_interpolate(follow1.global_position + approch_indexes[current_idx][2] , delta * FOLLOW_SPEEDS[current_idx])
-
+	
+	rotation = global_position.angle_to_point(get_parent().position) - PI /2
+	follow1.rotation = follow1.global_position.angle_to_point(global_position) - PI /2
+	follow2.rotation = follow2.global_position.angle_to_point(follow1.global_position) - PI /2
 		
 
 func _on_Timer_timeout():
-	print("Now resting!")
 	FOLLOW_SPEEDS[0] = RESTING_FOLLOW_SPEED
